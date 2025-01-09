9e38681f8371b25bb2b33621087379171d2ddd78
Jerome Infante <jeromeinfante0506@gmail.com>
2025-01-09 20:19:00 +0800

    Create evacuationManagement.dart

---
+++ lib/screen/evacuationManagement.dart
@@ -0,0 +1,16 @@
+// ignore: file_names
+import 'package:flutter/material.dart';
+
+class Evacuationmanagement extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Screen One',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
