658678de6d282c73006adf81d6ec4d755e5a662f
Jerome Infante <jeromeinfante.dev@gmail.com>
2025-01-24 08:36:03 +0800

    Update evacuationCenter.dart

--- lib/screen/evacuationCenter.dart
+++ lib/screen/evacuationCenter.dart
@@ -73,82 +73,151 @@ class _CalamityDetailsScreenState extends State<CalamityDetailsScreen> {
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 Container(
-                  height: 300.0,
-                  padding: EdgeInsets.all(15.0),
-                  decoration: BoxDecoration(
-                    color: Colors
-                        .blue[50], // Background color for the main container
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  child: Stack(
-                    children: [
-                      Align(
-                        alignment: Alignment
-                            .topLeft, // Position the first text at the upper-left
-                        child: Padding(
-                          padding: EdgeInsets.all(
-                              8.0), // Add some padding for better spacing
-                          child: Text(
-                            '${widget.calamityData['Calamity Name']}', // Accessing the map data correctly
-                            style: TextStyle(
-                              color: Colors.blue,
-                              fontSize: 30,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
+                    height: 300.0,
+                    padding: EdgeInsets.all(15.0),
+                    decoration: BoxDecoration(
+                      color: Colors
+                          .blue[50], // Background color for the main container
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Stack(
+                      children: [
+                        // Light blue background
+                        Container(
+                          color: Color(0xFFEAF1FF),
+                          width: double.infinity,
+                          height: double.infinity,
                         ),
-                      ),
-                      Align(
-                        alignment: Alignment
-                            .topLeft, // Position the second text below the first
-                        child: Padding(
-                          padding: EdgeInsets.only(
-                              left: 8.0, top: 60.0), // Adjust top padding
-                          child: Text(
-                            'An overview of evacuees',
-                            style: TextStyle(
-                              color: Colors.blue,
-                              fontSize: 18,
-                            ),
-                          ),
-                        ),
-                      ),
-                      Align(
-                        alignment: Alignment
-                            .center, // Position the new container at the center
-                        child: Container(
-                          width: double
-                              .infinity, // Make the container stretch horizontally
-                          height: 100.0, // Height of the new container
-                          decoration: BoxDecoration(
-                            color: Colors
-                                .white, // Background color for the new container
-                            borderRadius:
-                                BorderRadius.circular(10.0), // Rounded corners
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.grey
-                                    .withOpacity(0.5), // Shadow color
-                                spreadRadius: 2,
-                                blurRadius: 5,
-                                offset: Offset(0, 3), // Shadow position
+
+                        // Typhoon name and description
+                        Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'TYPHOON EGAY',
+                                style: TextStyle(
+                                  color: Colors.blue,
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              SizedBox(height: 8.0),
+                              Text(
+                                'An overview of evacuees',
+                                style: TextStyle(
+                                  color: Colors.blue,
+                                  fontSize: 18,
+                                ),
+                              ),
+                              SizedBox(height: 32.0), // Space for alignment
+                              Align(
+                                alignment: Alignment
+                                    .centerLeft, // Aligns horizontally at the center
+                                child: Padding(
+                                  padding: const EdgeInsets.only(
+                                      top: 16.0,
+                                      left: 90), // Fine-tune vertical spacing
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment
+                                        .center, // Center the children horizontally
+                                    children: [
+                                      Text(
+                                        'Overall Total of Evacuees', // Label text
+                                        style: TextStyle(
+                                          fontSize: 16.0,
+                                          fontWeight: FontWeight.normal,
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                      SizedBox(
+                                          height:
+                                              4.0), // Space between the label and the number
+                                      Text(
+                                        '64', // The number to display
+                                        style: TextStyle(
+                                          fontSize: 40.0,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
                               ),
                             ],
                           ),
-                          child: Center(
-                            child: Text(
-                              'Details about evacuees go here',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 16,
+                        ),
+
+                        // White container, centered and shifted towards the right
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: Container(
+                            width: MediaQuery.of(context).size.width *
+                                0.7, // Adjust width
+                            height: 300.0,
+                            margin: EdgeInsets.only(
+                                left: 32.0,
+                                right: 16.0), // Create space on the left
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10.0),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.5),
+                                  spreadRadius: 2,
+                                  blurRadius: 5,
+                                  offset: Offset(0, 3), // Shadow position
+                                ),
+                              ],
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: GridView.builder(
+                                itemCount: 12, // 4 rows * 3 columns
+                                gridDelegate:
+                                    SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 4, // 3 items per row
+                                  childAspectRatio:
+                                      4, // Adjust the aspect ratio to make it look like rows
+                                  mainAxisSpacing:
+                                      1.0, // Reduce the spacing between rows
+                                  crossAxisSpacing:
+                                      2.0, // Reduce the spacing between columns
+                                ),
+                                itemBuilder: (context, index) {
+                                  return Row(
+                                    children: [
+                                      Text(
+                                        '${index + 1}', // Number on the left side
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      SizedBox(
+                                          width:
+                                              4.0), // Reduce the spacing between number and description
+                                      Expanded(
+                                        child: Text(
+                                          'Description ${index + 1}', // Description on the right side
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 16,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  );
+                                },
                               ),
                             ),
                           ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+                        )
+                      ],
+                    )),
 
                 SizedBox(height: 20.0), // Space between the two boxes
                 Container(
@@ -430,7 +499,8 @@ class _CalamityDetailsScreenState extends State<CalamityDetailsScreen> {
                           itemBuilder: (context, index) {
                             final data = datas[index];
                             return Card(
-                              margin: EdgeInsets.all(15.0),
+                              margin: EdgeInsets.only(
+                                  bottom: 10.0, right: 10.0, left: 10.0),
                               shape: RoundedRectangleBorder(
                                 borderRadius: BorderRadius.circular(8.0),
                               ),
